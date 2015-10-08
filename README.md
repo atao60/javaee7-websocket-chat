42d0bad6d88c2bd8934fe7e2bfbdc9361bc8bfcf
atao60 <p.m.raoul@gmail.com>
2015-10-08 11:32:58 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Java EE 7 - GlassFish WebSockets Chat Tutorial
+
+An example setting up a websocket-enabled chat system using Java EE 7, custom encoders/decoders, GlassFish 4, the new Java API for JSON Processing and Bootstrap.
+
+For more detailed information, please feel free to have a look at my blog at [www.hascode.com].
+
+----
+
+**2013 Micha Kops / hasCode.com**
+
+   [www.hascode.com]:http://www.hascode.com/
+
+
+## Running
+
+      mvn clean package embedded-glassfish:run
+   
+Go to
+    http://localhost:8084/hascode   
+    
+Note. The port can be changed from the pom file.    
+    
+## Resources
+
+[1] Creating a Chat Application using Java EE 7, Websockets and GlassFish 4  
+>  13/08/2013  
+>  http://www.hascode.com/2013/08/creating-a-chat-application-using-java-ee-7-websockets-and-glassfish-4/
+
+>Code source:   
+>>      Java EE 7 - GlassFish WebSockets Chat Tutorial  
+>>      https://bitbucket.org/hascode/javaee7-websocket-chat/
+
+    
---
+++ src/main/java/com/hascode/tutorial/ChatEndpoint.java
@@ -0,0 +1,38 @@
+package com.hascode.tutorial;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.websocket.EncodeException;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/chat/{room}", encoders = ChatMessageEncoder.class, decoders = ChatMessageDecoder.class)
+public class ChatEndpoint {
+	private final Logger log = Logger.getLogger(getClass().getName());
+
+	@OnOpen
+	public void open(final Session session, @PathParam("room") final String room) {
+		log.info("session openend and bound to room: " + room);
+		session.getUserProperties().put("room", room);
+	}
+
+	@OnMessage
+	public void onMessage(final Session session, final ChatMessage chatMessage) {
+		String room = (String) session.getUserProperties().get("room");
+		try {
+			for (Session s : session.getOpenSessions()) {
+				if (s.isOpen()
+						&& room.equals(s.getUserProperties().get("room"))) {
+					s.getBasicRemote().sendObject(chatMessage);
+				}
+			}
+		} catch (IOException | EncodeException e) {
+			log.log(Level.WARNING, "onMessage failed", e);
+		}
+	}
+}
